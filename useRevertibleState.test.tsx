c70409430c487c8d03f588911535425957f42588
A-yon Lee <i@hyurl.com>
2024-11-09 00:08:38 +0800

    rename hook

---
+++ useRevertibleState.test.tsx
@@ -0,0 +1,85 @@
+import { describe, expect, test } from "vitest"
+import { renderHook } from "./testing.tsx"
+import useRevertibleState from "./useRevertibleState.ts"
+import { act } from "@testing-library/react"
+
+describe("useRevertibleState", () => {
+    test("initial state", () => {
+        const { result } = renderHook(() => useRevertibleState(""))
+        const [state] = result.current
+        expect(state).toBe("")
+    })
+
+    test("set state", () => {
+        const { result } = renderHook(() => useRevertibleState(""))
+        const [, setState] = result.current
+        act(() => setState("foo"))
+        const [state] = result.current
+        expect(state).toBe("foo")
+    })
+
+    test("undo", () => {
+        const { result } = renderHook(() => useRevertibleState(""))
+
+        let [, setState] = result.current
+        act(() => setState("foo"))
+        expect(result.current[0]).toBe("foo");
+
+        [, setState] = result.current
+        act(() => setState("bar"))
+        expect(result.current[0]).toBe("bar")
+
+        act(() => result.current[2].undo())
+        expect(result.current[0]).toBe("foo")
+
+        act(() => result.current[2].undo())
+        expect(result.current[0]).toBe("")
+    })
+
+    test("redo", () => {
+        const { result } = renderHook(() => useRevertibleState(""))
+
+        act(() => result.current[1]("foo"))
+        expect(result.current[0]).toBe("foo")
+
+        act(() => result.current[1]("bar"))
+        expect(result.current[0]).toBe("bar")
+
+        act(() => result.current[2].undo())
+        expect(result.current[0]).toBe("foo")
+
+        act(() => result.current[2].undo())
+        expect(result.current[0]).toBe("")
+
+        act(() => result.current[2].redo())
+        expect(result.current[0]).toBe("foo")
+
+        act(() => result.current[2].redo())
+        expect(result.current[0]).toBe("bar")
+
+        act(() => result.current[2].redo())
+        expect(result.current[0]).toBe("bar")
+    })
+
+    test("update history after setState", () => {
+        const { result } = renderHook(() => useRevertibleState(""))
+
+        act(() => result.current[1]("foo"))
+        expect(result.current[0]).toBe("foo")
+
+        act(() => result.current[1]("bar"))
+        expect(result.current[0]).toBe("bar")
+
+        act(() => result.current[2].undo())
+        expect(result.current[0]).toBe("foo")
+
+        act(() => result.current[1]("baz"))
+        expect(result.current[0]).toBe("baz")
+
+        act(() => result.current[2].redo())
+        expect(result.current[0]).toBe("baz")
+
+        act(() => result.current[2].undo())
+        expect(result.current[0]).toBe("foo")
+    })
+})
---
+++ useRevertibleState.ts
@@ -0,0 +1,67 @@
+import { Dispatch, SetStateAction, useCallback, useRef, useState } from "react"
+
+/**
+ * This hook adds additional functions to the `useState` hook, allowing us to
+ * undo and redo state changes.
+ * 
+ * ```tsx
+ * import useRevertibleState from "./useRevertibleState.ts"
+ * 
+ * export default function MyComponent() {
+ *     const [text, setText, { undo, redo }] = useRevertibleState("")
+ * 
+ *     return (
+ *         <div>
+ *             <input value={text} onChange={e => setText(e.target.value)} />
+ *             <button onClick={undo}>Undo</button>
+ *             <button onClick={redo}>Redo</button>
+ *         </div>
+ *     )
+ * }
+ * ```
+ */
+export default function useRevertibleState<T>(initial: T): [T, Dispatch<SetStateAction<T>>, {
+    undo: () => void
+    redo: () => void
+    clear: () => void
+}] {
+    const [state, setState] = useState(initial)
+    const [history, setHistory] = useState([initial])
+    const index = useRef(0)
+
+    return [
+        state,
+        useCallback((newState) => {
+            if (typeof newState === "function") {
+                newState = (newState as (prev: T) => T)(state)
+            }
+
+            let _history = history
+            if (index.current < _history.length - 1) {
+                _history = _history.slice(0, index.current + 1)
+            }
+
+            index.current++
+            setState(newState)
+            setHistory([..._history, newState])
+        }, [state, history, setState, setHistory]),
+        {
+            undo: useCallback(() => {
+                if (index.current > 0) {
+                    index.current--
+                    setState(history[index.current])
+                }
+            }, [history, setState]),
+            redo: useCallback(() => {
+                if (index.current < history.length - 1) {
+                    index.current++
+                    setState(history[index.current])
+                }
+            }, [history, setState]),
+            clear: useCallback(() => {
+                index.current = 0
+                setState(initial)
+                setHistory([initial])
+            }, [initial, setState, setHistory]),
+        }]
+}
